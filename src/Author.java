0ae35e423c90bd5c5f8d11ae898ba41e57734b4f
Роман <romanze1706@gmail.com>
2022-11-21 17:10:04 +0300

    Author and book

---
+++ src/Author.java
@@ -0,0 +1,10 @@
+public class Author {
+    String firstName;
+    String lastName;
+
+    public Author (String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+}
---
+++ src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    String bookName;
+    String author;
+    int createYear;
+
+    public Book (String bookName, String author, int createYear) {
+        this.bookName = bookName;
+        this.author = author;
+        this.createYear = createYear;
+    }
+}
